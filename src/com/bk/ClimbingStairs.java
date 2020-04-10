81f85d54ca95f43717e8358ce25009946ab22695
bchaudhari <bhaveshkumar.chaudhari@gmail.com>
2020-04-09 22:34:17 -0700

    Leetcode - https://leetcode.com/problems/climbing-stairs/
    Improved version.

--- src/com/bk/ClimbingStairs.java
+++ src/com/bk/ClimbingStairs.java
@@ -31,21 +31,20 @@ Explanation: There are three ways to climb to the top.
 public class ClimbingStairs {
 	
 	public int climbStairs(int n) {
-		int result = 0;
-        if(n == 1) {
-        	result = 1;
+		int result[] = new int[n+2];
+        result[1] = 1;
+        result[2] = 2;
+        if(n > 2) {
+	        for(int i = 3; i <= n; i++) {
+	        	result[i] = result[i - 1] + result[i - 2];
+	        }
         }
-        else if(n == 2) {
-        	result = 2;
-        } else {
-        	result = climbStairs(n - 1) + climbStairs(n - 2);
-        }
-        return result;       
+        return result[n];       
     }
 
 	public static void main(String[] args) {
 		ClimbingStairs climbingStairs = new ClimbingStairs();
-		System.out.println(climbingStairs.climbStairs(45));
+		System.out.println(climbingStairs.climbStairs(2));
 	}
 
 }
