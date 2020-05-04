25ac4f0b75ce2e2fa4ada2a98bd7f7d0df5a0d7e
bchaudhari <bhaveshkumar.chaudhari@gmail.com>
2020-05-04 10:39:08 -0700

    Leetcode - https://leetcode.com/problems/valid-anagram/

---
+++ src/com/bk/ValidAnagram.java
@@ -0,0 +1,59 @@
+package com.bk;
+
+import java.util.Arrays;
+
+/***
+ * Leetcode - https://leetcode.com/problems/valid-anagram/
+ * 
+ Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ 
+ * @author bhaveshkumar.chaudhari@gmail.com
+ *
+ */
+public class ValidAnagram {
+	
+	public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] sa = s.toLowerCase().toCharArray(); // Convert to lower case to handle mix case scenarios
+        char[] ta = t.toLowerCase().toCharArray(); // Convert to lower case to handle mix case scenarios
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+        
+        return new String(sa).equals(new String(ta));
+    }
+	
+	public static void main(String[] args) {
+		ValidAnagram anagram = new ValidAnagram();
+		
+		String s1 = "anagram";
+		String t1 = "nagaram";
+		
+		String s2 = "DebitCard";
+		String t2 = "Badcredit";
+		
+		String s3 = "abcd";
+		String t3 = "abc";
+		
+		String s4 = "xyz";
+		String t4 = "yxy";
+		
+		System.out.println("Expected true found --> " + anagram.isAnagram(s1, t1));
+		System.out.println("Expected true found --> " + anagram.isAnagram(s2, t2));
+		System.out.println("Expected false found --> " + anagram.isAnagram(s3, t3));
+		System.out.println("Expected false found --> " + anagram.isAnagram(s4, t4));
+	}
+}
