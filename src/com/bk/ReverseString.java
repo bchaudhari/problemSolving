db263b592fd719b51ce39ea291bc0cbd971e8db6
bchaudhari <bhaveshkumar.chaudhari@gmail.com>
2020-05-08 20:46:06 -0700

    Leetcode - https://leetcode.com/problems/reverse-string/

---
+++ src/com/bk/ReverseString.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.bk;
+
+/**
+ * Leetcode - https://leetcode.com/problems/reverse-string/
+ * 
+ Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+ * @author bhaveshkumar.chaudhari@gmail.com
+ *
+ */
+public class ReverseString {
+
+	public void reverseString(char[] s) {
+		char temp;
+		int left = 0;
+		int right = s.length - 1;
+		
+		while(left < right) {
+			temp = s[right];
+			s[right] = s[left];
+			s[left] = temp;
+			right--;
+			left++;
+		}	
+	}
+	public static void main(String[] args) {
+		char[] s1 = {'h','e','l','l','o'};
+		ReverseString revString = new ReverseString();
+		revString.reverseString(s1);
+		printArray(s1);
+		
+		char[] s2 = {'h','a','n','n','a','h'};
+		revString.reverseString(s2);
+		System.out.println("\n");
+		printArray(s2);
+	}
+	
+	public static void printArray(char[] s) {
+		for(char c: s) {
+			System.out.print(c + " ");
+		}
+	}
+	
+	
+
+}
