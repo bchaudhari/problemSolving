7e1f2d98baa47a8b3993b2ce654070c2eac051d3
bchaudhari <bhaveshkumar.chaudhari@gmail.com>
2020-05-08 20:26:04 -0700

    Leetcode - https://leetcode.com/problems/delete-node-in-a-linked-list/

---
+++ src/com/bk/DeleteLinkNodeWithoutHeadAccess.java
@@ -0,0 +1,58 @@
+package com.bk;
+
+/***
+ * Leetcode - https://leetcode.com/problems/delete-node-in-a-linked-list/
+ * 
+ Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Given linked list -- head = [4,5,1,9], which looks like following:
+Example 1:
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+Example 2:
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+
+ * @author bhaveshkumar.chaudhari@gmail.com
+ *
+ */
+public class DeleteLinkNodeWithoutHeadAccess {
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode one = new ListNode(4);
+		ListNode two = new ListNode(5);
+		ListNode tree = new ListNode(1);
+		ListNode four = new ListNode(9);
+		
+		one.next = two;
+		two.next = tree;
+		tree.next = four;
+		four.next = null;
+		
+		DeleteLinkNodeWithoutHeadAccess deleteLinkNodeWithoutHeadAccess = new DeleteLinkNodeWithoutHeadAccess();
+		deleteLinkNodeWithoutHeadAccess.deleteNode(two);
+		printLinkedList(one);
+		
+	}
+	
+	public static void printLinkedList(ListNode node) {
+		while(node != null) {
+			System.out.print(node.val + " ");
+			node = node.next;
+		}
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
