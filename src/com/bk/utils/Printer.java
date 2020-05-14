1d44a0ea3b510fd60a75a5138e3ded0f5af141f9
bchaudhari <bhaveshkumar.chaudhari@gmail.com>
2020-05-13 17:44:47 -0700

    Minor changes in print util

--- src/com/bk/utils/Printer.java
+++ src/com/bk/utils/Printer.java
@@ -5,19 +5,21 @@ public class Printer {
 		System.out.print("\n[");
 		for(int i = 0; i < arr.length; i++) {
 			if(i == arr.length - 1)
-				System.out.print(arr[i] + "]");
+				System.out.print(arr[i]);
 			else 
 				System.out.print(arr[i] + ",");
 		}
+		System.out.print("]");
 	}
 	
 	public static void printArray(char[] arr) {
 		System.out.print("\n[");
 		for(int i = 0; i < arr.length; i++) {
 			if(i == arr.length - 1)
-				System.out.print(arr[i] + "]");
+				System.out.print(arr[i]);
 			else 
 				System.out.print(arr[i] + ",");
 		}
+		System.out.print("]");
 	}
 }
